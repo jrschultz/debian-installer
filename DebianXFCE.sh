e62ae1e8d0769699e7ddd23695dbac8f91dcc780
Jason Schultz <42095568+jrschultz@users.noreply.github.com>
2024-04-23 17:28:11 -0600

    Add files via upload
    More stuff added

--- DebianXFCE.sh
+++ DebianXFCE.sh
@@ -15,7 +15,7 @@ sudo apt install -y dialog mtools acpi acpid gvfs-backends
 # Essentials:
 # ===========
 #
-sudo apt install -y nala neovim rsync curl git pandoc zsh ranger gparted openssh-server bpytop neofetch kdeconnect mpv ffmpeg samba gnome-disk-utility gimp hunspell-en-ca plank kate scrot i3lock exa evince
+sudo apt install -y nala neovim rsync curl git pandoc zsh ranger gparted openssh-server bpytop neofetch kdeconnect mpv ffmpeg samba gnome-disk-utility gimp hunspell-en-ca plank kate scrot i3lock exa evince tilix
 #
 # EXA installation (a better version of ls) replace ls command in .zshrc file with line below
 # alias ls='exa --color=always --group-directories-first' 
@@ -36,17 +36,17 @@ sudo apt install flatpak -y && flatpak remote-add --if-not-exists flathub https:
 #
 # Flatpaks:
 # ====================
-flatpak install Brave Chromium Chrome Bitwarden Flatseal Zoom
+flatpak install Brave Chromium Chrome Bitwarden Flatseal Zoom trustedqsl
 #
 # Latex:
 # ======
 # sudo apt install -y texlive-full texlive-lang-all texlive-luatex texlive-xetex texlive-fonts-extra
-
+#
 # Other Sources:
 # ==============
 # OhMyZsh: sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # ytdlp: https://github.com/yt-dlp/yt-dlp
-
+#
 # Theming:
 # ========
 # theme: arc-theme
@@ -55,6 +55,11 @@ flatpak install Brave Chromium Chrome Bitwarden Flatseal Zoom
 # kde: kde-style-breeze (for kate editor)
 #
 sudo apt install -y arc-theme papirus-icon-theme breeze-cursor-theme kde-style-breeze
+#
+# SlickGreeter:
+# =============
+sudo apt install slick-greeter
+#
 #
 # Ham Radio:
 # ==========
@@ -67,6 +72,17 @@ sudo apt install -y flrig fldigi wsjtx wsjtx-data wsjtx-doc js8call ax25-apps ax
 #
 # *Enable the greeter*
 # sudo systemctl enable lightdm
+#
+# *Use Slick Greeter*
+# cd /etc/lightdm
+# sudo nvim lightdm.conf
+# find #greeter-session= and make it greeter-session=slick-greeter
+# Line below should be greeter-hide-users=false
+# Change the background: sudo cp picture.jpg /usr/share/backgrounds/slickback.jpg
+# cd /etc/lightdm && sudo nvim slick-greeter.conf
+# [Greeter]
+# draw-user-backgrounds=false
+# background=/usr/share/backgrounds/slickback.jpg
 #
 # *Enable the network stuff*
 # sudo systemctl enable avahi-daemon && sudo systemctl enable acpid
